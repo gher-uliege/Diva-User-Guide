4600acb43eacca90addee09b475d0b2d741f9e5c
Charles Troupin <charles.troupin@gmail.com>
2012-10-24 12:30:06 +0000

    Initial repository layout

---
+++ figures/images/kernel.m
@@ -0,0 +1,36 @@
+% plot of the Kernel function K1(r/L) and 
+% the result of a single point analysis
+%------------------------------------------
+
+clear, close all, clc
+%--------------------
+
+% load the results (gg), the spatial step (dx) and the size of the domain
+% in number of dx (imax)
+%--------------------------------------------------------------------------
+
+load OnePointAnalysis.mat
+
+N=(imax-1)/2;
+xx = [0:dx:N*dx];
+yy = gg(N+1,N+1:2*N+1);
+
+% plot of the analysed field versus the distance with respect to the data
+%------------------------------------------------------------------------
+
+figure;
+plot(xx,yy,'color','r','linewidth',1.5');
+hold  on;
+
+nu1 = 1;
+besselfun1 = besselk(nu1,xx);
+ordonnee1 = xx.*besselfun1;
+plot(xx,ordonnee1,'color','b','linewidth',1.5'); hold on
+
+
+grid on;
+xlabel('Distance (r/L)','fontsize',14)
+ylabel('Field value','fontsize',14)
+
+print('-djpeg','kernelf.jpg')
+print('-depsc2','kernelf.eps')
