06f6105133fc5d1db8748ddc1d62e6d5695fa93c
Charles Troupin <charles.troupin@gmail.com>
2013-01-18 17:06:19 +0000

    improved figures

--- figures/images/kernel.m
+++ figures/images/kernel.m
@@ -2,14 +2,14 @@
 % the result of a single point analysis
 %------------------------------------------
 
-clear, close all, clc
+clear all; close all; clc;
 %--------------------
 
 % load the results (gg), the spatial step (dx) and the size of the domain
 % in number of dx (imax)
 %--------------------------------------------------------------------------
 
-load OnePointAnalysis.mat
+load('./OnePointAnalysis.mat');
 
 N=(imax-1)/2;
 xx = [0:dx:N*dx];
@@ -19,18 +19,17 @@ yy = gg(N+1,N+1:2*N+1);
 %------------------------------------------------------------------------
 
 figure;
-plot(xx,yy,'color','r','linewidth',1.5');
+plot(xx,yy,'color','r','linewidth',2);
 hold  on;
 
 nu1 = 1;
 besselfun1 = besselk(nu1,xx);
 ordonnee1 = xx.*besselfun1;
-plot(xx,ordonnee1,'color','b','linewidth',1.5'); hold on
+plot(xx,ordonnee1,'color','k','linewidth',2); hold on
 
 
-grid on;
 xlabel('Distance (r/L)','fontsize',14)
-ylabel('Field value','fontsize',14)
+ylabel('Field value     ','fontsize',14,'rotation',0)
+set(gca,'fontsize',14);
 
-print('-djpeg','kernelf.jpg')
-print('-depsc2','kernelf.eps')
+exportfig(gcf,'kernelf','bounds','tight','Preview','none','Color','rgb','Resolution',300)
